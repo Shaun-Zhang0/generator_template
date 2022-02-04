619ae9260c47fdd8aa5c03d528210a641e85b648
Shaun-Zhang0 <724892908@qq.com>
2022-02-04 17:44:59 +0800

    feat: 初始化仓库

---
+++ src/pages/main/index.js
@@ -0,0 +1,8 @@
+import React, { PureComponent } from 'react';
+import styles from './index.less';
+
+export default class Main extends PureComponent {
+  render() {
+    return <div className={styles.root} />;
+  }
+}
